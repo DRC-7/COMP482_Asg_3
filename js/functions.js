7f17b98e5ecda077720b74068b901dff0836cc08
DRC-7 <d.chung1245@gmail.com>
2020-08-27 22:44:56 -0600

    Created function.js
    Collects form information and stores it in localStorage.

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,41 @@
-//Login Button
-let button = document.querySelectorAll('.button button-block');
-console.log(button)
+//Create Account Button
+document.querySelector('#createAccount').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    console.log(e)
+
+    let firstName = e.target.form[0].value;
+    localStorage.setItem('FirstName', firstName);
+
+    let lastName = e.target.form[1].value;
+    localStorage.setItem('LastName', lastName);
+
+    let email = e.target.form[2].value;
+    localStorage.setItem('Email', email);
+
+    let password = e.target.form[3].value;
+    localStorage.setItem('Password', password);
+
+    let address = e.target.form[4].value;
+    localStorage.setItem('Address', address);
+
+    let city = e.target.form[5].value;
+    localStorage.setItem('City', city);
+
+    let province = e.target.form[6].value;
+    localStorage.setItem('Province', province);
+
+    let postalcode = e.target.form[7].value;
+    localStorage.setItem('PostalCode', postalcode);
+
+    console.log(firstName)
+    console.log(lastName)
+    console.log(email)
+    console.log(password)
+    console.log(address)
+    console.log(city)
+    console.log(province)
+    console.log(postalcode)
+
+
+});
